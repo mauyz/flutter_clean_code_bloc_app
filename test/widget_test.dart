1d0c13f3235c1bba2c90349f848fa95a9f9d2f44
moisefeelin <moise@feelin.be>
2024-01-16 16:33:25 +0300

    create app directory

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,4 @@
-
-import 'package:cross_platform_app/app.dart';
+import 'package:cross_platform_app/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
