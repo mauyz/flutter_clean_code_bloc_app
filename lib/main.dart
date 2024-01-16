6b70ef80bd13c9033ab19a613326a0c520859bea
moisefeelin <moise@feelin.be>
2024-01-16 16:33:58 +0300

    call dependecy injection init method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:cross_platform_app/app.dart';
+import 'package:cross_platform_app/app/app.dart';
+import 'package:cross_platform_app/services/dependecy_injection.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  configureDependecies();
   runApp(
     const App(),
   );
