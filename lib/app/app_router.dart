6a571ab6bf3e70b4615e1a03d44375bd479a44a3
moisefeelin <moise@feelin.be>
2024-01-17 17:27:47 +0300

    update app routing

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,16 +1,21 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:cross_platform_app/presentation/home_page.dart';
+import 'package:cross_platform_app/presentation/dashboard/home_page.dart';
+import 'package:cross_platform_app/presentation/onboarding/login/screen/login_page.dart';
 
 part 'app_router.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+@AutoRouterConfig()
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes {
     return [
       AutoRoute(
+        page: LoginRoute.page,
+        path: "/",
+      ),
+      AutoRoute(
         page: HomeRoute.page,
-        initial: true,
+        path: "/home",
       ),
     ];
   }
