712978d5e144d853f1642e99e304fa83a18568e5
moisefeelin <moise@feelin.be>
2024-01-18 14:56:02 +0300

    add endpoint constant

--- lib/data/sources/remote/api/api_constants.dart
+++ lib/data/sources/remote/api/api_constants.dart
@@ -2,4 +2,6 @@ class ApiConstants {
   const ApiConstants._();
   static const baseUrl = "http://restapi.adequateshop.com/api";
   static const login = "/authaccount/login";
+  static const register = "/authaccount/registration";
+  static const getUserById = "/users/";
 }
