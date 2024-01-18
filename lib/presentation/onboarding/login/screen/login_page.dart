2a54eba593e0cec45fa92b590032e7890afeda9c
moisefeelin <moise@feelin.be>
2024-01-18 11:37:07 +0300

    call login usecase on loginPage

--- lib/presentation/onboarding/login/screen/login_page.dart
+++ lib/presentation/onboarding/login/screen/login_page.dart
@@ -1,8 +1,14 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cross_platform_app/domain/usecases/user/log_out_usecase.dart';
+import 'package:cross_platform_app/domain/usecases/user/login_usecase.dart';
+import 'package:cross_platform_app/presentation/onboarding/login/bloc/auth_bloc.dart';
 import 'package:cross_platform_app/presentation/widgets/button/submit_button.dart';
 import 'package:cross_platform_app/presentation/widgets/input/email_input.dart';
 import 'package:cross_platform_app/presentation/widgets/input/password_input.dart';
+import 'package:cross_platform_app/presentation/widgets/popup/display_snackbar.dart';
+import 'package:cross_platform_app/services/dependecy_injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 @RoutePage()
@@ -44,15 +50,51 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(
                   height: 30,
                 ),
-                SubmitButton(
-                  text: 'Log in',
-                  onPressed: () {
-                    if (form.valid) {
-                      //todo
-                    } else {
-                      form.markAllAsTouched();
-                    }
-                  },
+                BlocProvider<AuthBloc>(
+                  create: (_) => AuthBloc(
+                    loginUseCase: getIt.get<LoginUseCase>(),
+                    logOutUseCase: getIt.get<LogOutUseCase>(),
+                  ),
+                  child: BlocConsumer<AuthBloc, AuthState>(
+                    listener: (_, state) {
+                      if (state is AuthenticatedFailed) {
+                        displaySnackBar(
+                          context,
+                          state.failure.message,
+                          isErrorMessage: true,
+                        );
+                      }
+                      if (state is Authenticated) {
+                        displaySnackBar(
+                          context,
+                          state.user.name,
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is UnAuthenticated ||
+                          state is AuthenticatedFailed) {
+                        return SubmitButton(
+                          text: 'Log in',
+                          onPressed: () {
+                            if (form.valid) {
+                              final email = form.control('email').value;
+                              final password = form.control('password').value;
+                              context.read<AuthBloc>().add(
+                                    LogInEvent(
+                                      email: email,
+                                      password: password,
+                                    ),
+                                  );
+                            } else {
+                              form.markAllAsTouched();
+                            }
+                          },
+                        );
+                      }
+                      return const CircularProgressIndicator();
+                    },
+                  ),
                 ),
               ],
             ),
