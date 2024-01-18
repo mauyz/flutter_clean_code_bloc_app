99431789609d59acd45ad6e0101319564ce20690
moisefeelin <moise@feelin.be>
2024-01-18 14:56:47 +0300

    update exception class

--- lib/core/error/exceptions.dart
+++ lib/core/error/exceptions.dart
@@ -1,19 +1,9 @@
-import 'dart:io';
-
-class UnauthorizedException implements Exception {}
-
-class NotFoundException implements Exception {}
-
-class InternalErrorServerException implements Exception {}
-
-class InternetException implements IOException {}
-
 class UnknownException implements Exception {}
 
-class ServerException implements Exception {
+class ApiException implements Exception {
   final int code;
 
-  ServerException({
+  ApiException({
     required this.code,
   });
 }
