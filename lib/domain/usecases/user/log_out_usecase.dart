a639f11701294e31baaaefe184b1f4ac3a918751
moisefeelin <moise@feelin.be>
2024-01-18 14:57:30 +0300

    update import

--- lib/domain/usecases/user/log_out_usecase.dart
+++ lib/domain/usecases/user/log_out_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:cross_platform_app/core/typedef.dart';
 import 'package:cross_platform_app/core/usecase.dart';
-import 'package:cross_platform_app/domain/repositories/user_reposiory.dart';
+import 'package:cross_platform_app/domain/repositories/user_repository.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
