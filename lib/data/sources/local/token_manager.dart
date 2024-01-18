63adb529b4b330756538706ce03e1a2b6169fd52
moisefeelin <moise@feelin.be>
2024-01-18 17:27:27 +0300

    move token_manager class

---
+++ lib/data/sources/local/token_manager.dart
@@ -0,0 +1,31 @@
+import 'package:cross_platform_app/core/constants/key_constants.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+class TokenManager {
+  final FlutterSecureStorage secureStorage;
+  String? _accessToken;
+  TokenManager({
+    required this.secureStorage,
+  });
+
+  Future<String?> getAccessToken() async {
+    _accessToken ??= await secureStorage.read(key: KeyConstants.accessToken);
+    return _accessToken;
+  }
+
+  Future<bool> hasToken() async {
+    return (await getAccessToken()) != null;
+  }
+
+  Future saveAccessToken(String token) async {
+    _accessToken = token;
+    await secureStorage.write(key: KeyConstants.accessToken, value: token);
+  }
+
+  Future deleteToken() async {
+    _accessToken = null;
+    await secureStorage.delete(key: KeyConstants.accessToken);
+  }
+}
