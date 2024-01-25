2f700aef773fbd29fbf6cd6962c6b9de5fb2f6f3
moisefeelin <moise@feelin.be>
2024-01-25 09:58:56 +0300

    implement home drawer management

--- lib/presentation/dashboard/screen/home_drawer.dart
+++ lib/presentation/dashboard/screen/home_drawer.dart
@@ -1,7 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cross_platform_app/app/app_router.dart';
 import 'package:cross_platform_app/domain/entities/user.dart';
-import 'package:cross_platform_app/presentation/dashboard/screen/drawer_item.dart';
+import 'package:cross_platform_app/presentation/dashboard/bloc/navigation_cubit.dart';
+import 'package:cross_platform_app/presentation/dashboard/screen/drawer_item_widget.dart';
 import 'package:cross_platform_app/presentation/onboarding/login/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,37 +17,44 @@ class HomeDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerItem(
-            leadingIcon: Icons.group,
-            title: "Users",
-            onTap: () {
-              Scaffold.of(context).closeDrawer();
-              context.router.pushNamed("users");
-            },
-          ),
-          DrawerItem(
-            leadingIcon: Icons.person,
-            title: "Profile",
-            onTap: () {
-              Scaffold.of(context).closeDrawer();
-              context.router.push(
-                ProfileRoute(
-                  user: user,
-                ),
-              );
-            },
-          ),
-          DrawerItem(
-            leadingIcon: Icons.logout,
-            title: "LogOut",
-            onTap: () {
-              context.read<AuthBloc>().add(const LogOutEvent());
-            },
-          ),
-        ],
+      child: BlocBuilder<NavigationCubit, int>(
+        builder: (_, index) {
+          return ListView(
+            children: [
+              DrawerItemWidget(
+                leadingIcon: Icons.group,
+                title: "Users",
+                selected: index == 0,
+                onTap: () {
+                  navigateTo(context, const UsersListRoute(), 0);
+                },
+              ),
+              DrawerItemWidget(
+                leadingIcon: Icons.person,
+                title: "Profile",
+                selected: index == 1,
+                onTap: () {
+                  navigateTo(context, ProfileRoute(user: user), 1);
+                },
+              ),
+              DrawerItemWidget(
+                leadingIcon: Icons.logout,
+                title: "LogOut",
+                selected: false,
+                onTap: () {
+                  context.read<AuthBloc>().add(const LogOutEvent());
+                },
+              ),
+            ],
+          );
+        },
       ),
     );
+  }
+
+  void navigateTo(BuildContext context, PageRouteInfo pageRoute, int index) {
+    Scaffold.of(context).closeDrawer();
+    context.router.push(pageRoute);
+    context.read<NavigationCubit>().navigateTo(index);
   }
 }
