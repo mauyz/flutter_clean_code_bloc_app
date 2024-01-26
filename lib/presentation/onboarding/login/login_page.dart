f1e7f9acc26b723d0a7d187ba3e03bb0aa2000d2
moisefeelin <moise@feelin.be>
2024-01-26 11:37:51 +0300

    add card container on login && register page

--- lib/presentation/onboarding/login/login_page.dart
+++ lib/presentation/onboarding/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cross_platform_app/app/app_router.dart';
 import 'package:cross_platform_app/presentation/form/form_controls.dart';
 import 'package:cross_platform_app/presentation/onboarding/login/bloc/auth_bloc.dart';
+import 'package:cross_platform_app/presentation/responsive_widget.dart';
 import 'package:cross_platform_app/presentation/widgets/button/submit_button.dart';
 import 'package:cross_platform_app/presentation/widgets/input/email_input.dart';
 import 'package:cross_platform_app/presentation/widgets/input/password_input.dart';
@@ -25,78 +26,90 @@ class LoginPage extends StatelessWidget {
     );
     return Scaffold(
       body: Center(
-        child: ReactiveForm(
-          formGroup: form,
-          child: SizedBox(
-            width: 300,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const EmailInput(),
-                  const SizedBox(
-                    height: 20,
+        child: Card(
+          child: Padding(
+            padding: ResponsiveWidget.isMobile(context)
+                ? const EdgeInsets.all(20.0)
+                : const EdgeInsets.symmetric(
+                    horizontal: 100.0,
+                    vertical: 30,
                   ),
-                  const PasswordInput(),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  BlocProvider<AuthBloc>(
-                    create: (_) => getIt.get<AuthBloc>(),
-                    child: BlocConsumer<AuthBloc, AuthState>(
-                      listener: (_, state) {
-                        if (state is AuthenticatedFailed) {
-                          displaySnackBar(
-                            context,
-                            state.failure.message,
-                            isErrorMessage: true,
-                          );
-                        }
-                        if (state is Authenticated) {
-                          context.router.replace(HomeRoute(user: state.user));
-                        }
-                      },
-                      builder: (context, state) {
-                        if (state is UnAuthenticated ||
-                            state is AuthenticatedFailed) {
-                          return SubmitButton(
-                            text: 'Log in',
-                            onPressed: () {
-                              if (form.valid) {
-                                final email = form.control('email').value;
-                                final password = form.control('password').value;
-                                context.read<AuthBloc>().add(
-                                      LogInEvent(
-                                        email: email,
-                                        password: password,
-                                      ),
-                                    );
-                              } else {
-                                form.markAllAsTouched();
-                              }
-                            },
-                          );
-                        }
-                        return const CircularProgressIndicator();
-                      },
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      context.router.replace(const RegisterRoute());
-                    },
-                    child: const Text(
-                      'Register',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
+            child: SizedBox(
+              width: 300,
+              child: ReactiveForm(
+                formGroup: form,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const EmailInput(),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
+                      const PasswordInput(),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      BlocProvider<AuthBloc>(
+                        create: (_) => getIt.get<AuthBloc>(),
+                        child: BlocConsumer<AuthBloc, AuthState>(
+                          listener: (_, state) {
+                            if (state is AuthenticatedFailed) {
+                              displaySnackBar(
+                                context,
+                                state.failure.message,
+                                isErrorMessage: true,
+                              );
+                            }
+                            if (state is Authenticated) {
+                              context.router
+                                  .replace(HomeRoute(user: state.user));
+                            }
+                          },
+                          builder: (context, state) {
+                            if (state is UnAuthenticated ||
+                                state is AuthenticatedFailed) {
+                              return SubmitButton(
+                                text: 'Log in',
+                                onPressed: () {
+                                  if (form.valid) {
+                                    final email = form.control('email').value;
+                                    final password =
+                                        form.control('password').value;
+                                    context.read<AuthBloc>().add(
+                                          LogInEvent(
+                                            email: email,
+                                            password: password,
+                                          ),
+                                        );
+                                  } else {
+                                    form.markAllAsTouched();
+                                  }
+                                },
+                              );
+                            }
+                            return const CircularProgressIndicator();
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          context.router.replace(const RegisterRoute());
+                        },
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
