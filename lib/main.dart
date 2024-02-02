b33c4592c83cf927866dbd64d33b03f81898bb83
moisefeelin <moise@feelin.be>
2024-02-02 15:27:13 +0300

    add dartdoc package

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:cross_platform_app/app/app.dart';
 import 'package:cross_platform_app/services/dependency_injection.dart';
 import 'package:flutter/material.dart';
 
+/// Starts and initializes the application
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Initializes all dependecies injection.
   await configureDependecies();
   runApp(
     const App(),
